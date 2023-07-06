016d122c75e0566490a51c7497658806099cb170
ingjorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-07-06 13:42:58 -0600

    updated

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -9,6 +9,7 @@ function TodoCounter(){
   return (
   <div className='counterContainer'>
     <h2 className='TodoCounter'>Completed <span className='number'>{completedTodos}</span> of <span className='number'>{totalTodos}</span> TODO's</h2>
+    <progress value={completedTodos} max={totalTodos} className='bar'>hola</progress>
   </div>
   );
 }
